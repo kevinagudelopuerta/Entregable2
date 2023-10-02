9a6b8306bd642729bdd66b4b4f427f0d233110d0
kevinagudelopuerta <110616326+kevinagudelopuerta@users.noreply.github.com>
2023-10-01 19:58:01 -0500

    "primer commit"
    Subir el archivo del entregable 2

---
+++ Entregable2/basket/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+interface IJugador {
+    string Nombre { get; }
+    string Posicion { get; }
+    int Rendimiento { get; }
+}
+
+class Jugador : IJugador {
+    public string Nombre { get; private set; }
+    public string Posicion { get; private set; }
+    public int Rendimiento { get; private set; }
+
+    public Jugador(string nombre, string posicion, int rendimiento) {
+        Nombre = nombre;
+        Posicion = posicion;
+        Rendimiento = rendimiento;
+    }
+}
+
+class Equipo {
+    private List<IJugador> jugadores = new List<IJugador>();
+
+    public void MeterJugador(IJugador jugador) {
+        jugadores.Add(jugador);
+    }
+
+    public int CalcularPuntaje() {
+        return jugadores.Sum(jugador => jugador.Rendimiento);
+    }
+}
+
+class Program {
+    static void Main(string[] args) {
+        List<IJugador> ListaDeJugadores = new List<IJugador> {
+            new Jugador("LeBron James", "Ala-pívot", 10),
+            new Jugador("Kevin Durant", "Base", 8),
+            new Jugador("hormiga giannis", "Pívot", 6),
+            new Jugador("Stephen Curry", "Base", 9),
+            new Jugador("Michael Jordan", "Base", 10),
+            new Jugador("Luka Doncic", "Alero", 7)
+        };
+
+
+        Equipo equipo1 = new Equipo();
+        Equipo equipo2 = new Equipo();
+
+        Random random = new Random();
+        while (ListaDeJugadores.Count > 0) {
+            int x = random.Next(ListaDeJugadores.Count);
+            IJugador jugadorSeleccionado = ListaDeJugadores[x];
+            if (equipo1.CalcularPuntaje() <= equipo2.CalcularPuntaje())
+            {
+                equipo1.MeterJugador(jugadorSeleccionado);
+            }
+            else
+            {
+                equipo2.MeterJugador(jugadorSeleccionado);
+            }
+            ListaDeJugadores.RemoveAt(x);
+        }
+
+        int puntajeEquipo1 = equipo1.CalcularPuntaje();
+        int puntajeEquipo2 = equipo2.CalcularPuntaje();
+
+        if (puntajeEquipo1 > puntajeEquipo2) {
+            Console.WriteLine("El equipo 1 gana con un puntaje de " + puntajeEquipo1);
+        }
+        else if (puntajeEquipo2 > puntajeEquipo1) {
+            Console.WriteLine("El equipo 2 gana con un puntaje de " + puntajeEquipo2);
+        }
+        else {
+            Console.WriteLine("El partido termina en empate con un puntaje de " + puntajeEquipo1);
+        }
+    }
+}
---
+++ Entregable2/pokemones/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+public class Pokemon {
+    public string Nombre { get; set; }
+    public string Tipo { get; set; }
+    public int Ataque1 { get; set; }
+    public int Ataque2 { get; set; }
+    public int Ataque3 { get; set; }
+    public int Defensa { get; set; }
+
+    public int Atacar(){
+        Random random = new Random();
+        int ataqueSeleccionado = random.Next(1, 4);
+        int puntaje = 0;
+
+        switch (ataqueSeleccionado) {
+            case 1:
+                puntaje = Ataque1;
+                break;
+            case 2:
+                puntaje = Ataque2;
+                break;
+            case 3:
+                puntaje = Ataque3;
+                break;
+        }
+
+        int multiplicador = random.Next(0, 2);
+        return puntaje * multiplicador;
+    }
+
+    public double Defender(){
+        Random random = new Random();
+        double multiplicador = random.NextDouble() < 0.5 ? 0.5 : 1;
+        return Defensa * multiplicador;
+    }
+}
+
+public class Batalla {
+    public static void Main(string[] args) {
+        Console.WriteLine("Ingrese los valores para el Pokémon 1:");
+        Pokemon pokemon1 = CrearPokemon();
+
+        Console.WriteLine("Ingrese los valores para el Pokémon 2:");
+        Pokemon pokemon2 = CrearPokemon();
+
+        Console.WriteLine("¡Comienza la batalla!");
+
+        int resultado1 = 0;
+        int resultado2 = 0;
+
+        for (int i = 1; i <= 3; i++) {
+            Console.WriteLine($"Turno {i}:");
+
+            Console.WriteLine($"El Pokémon 1 ataca:");
+            resultado1 += pokemon1.Atacar();
+            Console.WriteLine($"El Pokémon 2 defiende con una defensa de {pokemon2.Defender()}");
+
+            Console.WriteLine($"El Pokémon 2 ataca:");
+            resultado2 += pokemon2.Atacar();
+            Console.WriteLine($"El Pokémon 1 defiende con una defensa de {pokemon1.Defender()}");
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Termino batalla");
+
+        if (resultado1 > resultado2) {
+            Console.WriteLine("Pokémon 1 gana");
+        }
+        else if (resultado1 < resultado2) {
+            Console.WriteLine("Pokémon 2 gana");
+        }
+        else {
+            Console.WriteLine("Empate!");
+        }
+    }
+
+    public static Pokemon CrearPokemon() {
+        Pokemon pokemon = new Pokemon();
+
+        Console.Write("Nombre: ");
+        pokemon.Nombre = Console.ReadLine();
+
+        Console.Write("Tipo: ");
+        pokemon.Tipo = Console.ReadLine();
+
+        Console.Write("Ataque 1 (entre 0 y 40): ");
+        pokemon.Ataque1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Ataque 2 (entre 0 y 40): ");
+        pokemon.Ataque2 = int.Parse(Console.ReadLine());
+
+        Console.Write("Ataque 3 (entre 0 y 40): ");
+        pokemon.Ataque3 = int.Parse(Console.ReadLine());
+
+        Console.Write("Defensa (entre 10 y 35): ");
+        pokemon.Defensa = int.Parse(Console.ReadLine());
+
+        Console.WriteLine();
+
+        return pokemon;
+    }
+}
+
+
+
+
